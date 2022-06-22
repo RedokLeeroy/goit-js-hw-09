68cabb216b99e52432fffb751a1d09c944d0b5c8
RedokLeeroy <Lys1999@ukr.net>
2022-06-22 22:30:41 +0300

    .

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,11 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const body = document.querySelector("body");
+const startBtn = document.querySelector("button[data-start]");
+
+startBtn.addEventListener("click", handleStart)
+function handleStart() {
+    setInterval(() =>body.style.backgroundColor = getRandomHexColor(), 1000)
+}
