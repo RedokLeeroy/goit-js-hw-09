f0a8b0edd9a00b87356414a382693776761aeae2
RedokLeeroy <Lys1999@ukr.net>
2022-06-22 19:31:07 +0000

    Deploying to gh-pages from @ RedokLeeroy/goit-js-hw-09@68cabb216b99e52432fffb751a1d09c944d0b5c8 🚀

---
+++ 01-color-switcher.91259722.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body");document.querySelector("button[data-start]").addEventListener("click",(function(){setInterval((function(){return t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)}))}();
+//# sourceMappingURL=01-color-switcher.91259722.js.map
