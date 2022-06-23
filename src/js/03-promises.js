2617e5c129d74f851980984eadce36930a5f091b
RedokLeeroy <Lys1999@ukr.net>
2022-06-23 22:11:44 +0300

    .

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.5.min.css';
+
+const delay = document.querySelector("[name = 'delay']");
+const step = document.querySelector("[name = 'step']");
+const amount = document.querySelector("[name = 'amount']");
+const form = document.querySelector(".form");
+
+function createPromise(index, delay) {
+  return new Promise ((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(()=> {  
   if (shouldResolve) {
-    // Fulfill
+    resolve({index, delay})
   } else {
-    // Reject
+    reject({index, delay})
   }
+}, delay)
+});
+}
+
+form.addEventListener("submit", handlePromise)
+
+function handlePromise(event) {
+  event.preventDefault();
+  let newdelay =  Number(delay.value)
+  for (let index = 0; index < amount.value; index++) {
+    newdelay += Number(step.value)
+    
+    createPromise(index, newdelay)
+    .then(({ index, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${index} in ${delay}ms`);
+    })
+    .catch(({ index, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${index} in ${delay}ms`);
+    });
+  }
+
+  event.target.reset()
 }
